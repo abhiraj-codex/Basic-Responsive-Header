0a81c7a2b767ee039414575f6462f219bd17db5a
01abhirajkumar01-creator <01.abhiraj.kumar.01@gmail.com>
2026-03-03 14:17:28 +0530

    MOBILE_FIRST_READY❤️

---
+++ script.js
@@ -0,0 +1,16 @@
+const menuButton = document.querySelector('.menu-btn');
+const closeButton = document.querySelector('.close-btn')
+const navMenu = document.querySelector('.nav-item');
+
+function lets(){
+menuButton.classList.toggle('active');
+closeButton.classList.toggle('no-active');
+navMenu.classList.toggle('nooo-active');
+
+
+}
+
+
+
+
+
